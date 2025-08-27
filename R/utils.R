dc090e25f421e04709acb8e9cc1ee580dc4622de
aclark02-arcus <aclark02@arcusbio.com>
2025-08-27 17:01:40 +0000

    creat initial 'val.filter' replica with current toolsets

--- R/utils.R
+++ R/utils.R
@@ -3,6 +3,11 @@
 #' Remove .recording attribute from all elements of the assessment,
 #'   whilst maintaining classes
 #' 
+#' @examples
+#' # riskmetric::pkg_ref("zoo", source = "pkg_cran_remote") |>
+#' #   riskmetric::pkg_assess() |>
+#' #   strip_recording()
+#' 
 #' @keywords internal
 strip_recording <- function(assessment) {
   no_record <-
@@ -18,3 +23,14 @@ strip_recording <- function(assessment) {
   class(no_record) <- class(assessment)
   no_record
 }
+
+# t<- riskmetric::pkg_ref("zoo", source = "pkg_cran_remote") |>
+#   riskmetric::pkg_assess() |>
+#   strip_recording()
+# names(t)
+# t$remote_checks |> attributes() # see .recording
+# f <- t |>
+#   strip_recording()
+# f$remote_checks
+# f$remote_checks |> attributes() # It's gone
+
